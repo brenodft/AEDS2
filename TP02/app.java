af3c0606deab80e73eb7900525074d88946b38ca
Breno Pires Santos <123776858+brenodft@users.noreply.github.com>
2024-04-11 23:55:51 -0300

    Delete TP02/app.java

--- TP02/app.java
+++
@@ -1,8 +0,0 @@
-public class app {
-    public static void main(String[] args){
-        Personagem teste = new Personagem();
-        String id = "efa802c8-ae18-4ae1-a524-49df21d05939";
-        teste.ler(id);
-        teste.imprimir();
-    }
-}
